726e0c08ef45eb96e9341fde38c37f05bfe97a63
Данил Татуйко <dtatuyko@gmail.com>
2026-07-18 17:33:14 +0200

    initialize identity service with PostgreSQL

---
+++ backend/identity-service/src/main/java/com/wokioki/parcelflow/identity/IdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wokioki.parcelflow.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServiceApplication.class, args);
+    }
+
+}
