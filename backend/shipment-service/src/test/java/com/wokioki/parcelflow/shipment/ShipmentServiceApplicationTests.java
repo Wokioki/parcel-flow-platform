5a2f4176e312df1bab62405fca9fba3b5daf7f19
Данил Татуйко <dtatuyko@gmail.com>
2026-09-01 01:26:51 +0200

    bootstrap shipment service

---
+++ backend/shipment-service/src/test/java/com/wokioki/parcelflow/shipment/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wokioki.parcelflow.shipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
